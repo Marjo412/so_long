728475f0bef456be14f150836fb009eb1de4b1e1
Marjorie Rosset <marjorie_4.28@hotmail.com>
2025-02-02 18:59:31 +0100

    Update map_check.c

--- map_check.c
+++ map_check.c
@@ -11,3 +11,17 @@
 /* ************************************************************************** */
 
 #include "includes/so_long.h"
+
+void  free_map(char **map)
+{
+  int  i;
+
+  i = 0;
+  while (map[i])
+    {
+      free(map[i]);
+      i++;
+    }
+  free(map);
+  map = NULL;
+}
